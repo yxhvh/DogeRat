8d58305ba3bbf842aa1a9b35d6d77ba1b58d8c38
yxhvh <kumarisoni45770@gmail.com>
2025-04-26 12:52:10 +0530

    Update port.sh

--- port.sh
+++ port.sh
@@ -16,5 +16,4 @@ echo "Author: shivayadv & Did-Dog"
 echo "Github: shivaya-dav & did-dog"
 echo "Telegram channel: cybershieldx"
 read -p "Enter your Telegram username: " telegram_username
-sed -i "0,/telegram_username/s//${telegram_username}/" port.sh
 ssh -R ${telegram_username}:80:localhost:8999 -o GatewayPorts=no serveo.net
